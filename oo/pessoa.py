15b430832d5423a3bda1088c1d5aa4c7795b72cf
VictorCarvalho16 <victor.inf2011@hotmail.com>
2019-10-31 12:00:50 -0300

    Criada atributos Complexo Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=22):
+    def __init__(self, *filhos, nome=None, idade=22):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
 
     def cumprimentar(self):
@@ -9,10 +10,12 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Victor')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
