ba960446422db988cdfcf16c6875e24e1e4db790
VictorCarvalho16 <victor.inf2011@hotmail.com>
2019-11-07 16:50:32 -0200

    implementação do lançamento horizontal

--- atores.py
+++ atores.py
@@ -125,6 +125,7 @@ class Passaro(Ator):
         if self.foi_lancado():
             delta_t = tempo - self._tempo_de_lancamento
             self.calcular_posicao_vertical(delta_t)
+            self.calcular_posicao_horizontal(delta_t)
         return super().calcular_posicao(tempo)
 
 
@@ -141,13 +142,17 @@ class Passaro(Ator):
         self._tempo_de_lancamento = tempo_de_lancamento
 
     def calcular_posicao_vertical(self, delta_t):
-        angulo_radianos = math.radians(self._angulo_de_lancamento)
         y_atual = self._y_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
         y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
         y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
         self.y = y_atual
 
-
+    def calcular_posicao_horizontal(self, delta_t):
+        x_atual = self._x_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        x_atual += self.velocidade_escalar * delta_t * math.cos(angulo_radianos)
+        self.x = x_atual
 
 
 class PassaroAmarelo(Passaro):
